58b26d55ed8f2b55d75da9c51d042a6fa1dac2fc
Md. Ibrahim Mamun <67334936+mamun-104@users.noreply.github.com>
2022-04-28 17:19:41 +0100

    Add files via upload

---
+++ 50A Domino piling.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int m,n;
+    cin>>m>>n;
+    int area;
+    area = m*n;
+
+    int result;
+    result = area/2;
+
+cout<<result<<endl;
+
+
+
+    return 0;
+}
